0afc37654418dc1f5e257975cc0ace094e887f27
Isaac Diamond <isaac+github@planetscale.com>
2026-04-02 11:04:38 -0700

    remove padding

--- kexp-menubar/kexp-menubar/ContentViewCommon.swift
+++ kexp-menubar/kexp-menubar/ContentViewCommon.swift
@@ -273,7 +273,6 @@ struct PlaylistScrollView: View {
                 }
             }
             .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.trailing, 20)
         }
         .scrollIndicators(.visible)
         .frame(maxHeight: maxHeight, alignment: .top)
